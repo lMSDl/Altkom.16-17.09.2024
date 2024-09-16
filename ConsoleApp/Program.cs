009fab94dd6ffbfe7094ea3d380d9d1ba7a8e0be
Paweł Łęczycki <pawel@msd.info.pl>
2024-09-16 09:05:18 +0200

    Przygotowanie projektu

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using DAL;
+using Microsoft.EntityFrameworkCore;
+
+var configurationOptions = new DbContextOptionsBuilder<Context>()
+    .UseSqlServer(@"Server=(local);Database=EfCore;Integrated security=true;TrustServerCertificate=true")
+    .Options;
+
+using (var context = new Context(configurationOptions))
+{
+    context.Database.EnsureCreated();
+
+}
